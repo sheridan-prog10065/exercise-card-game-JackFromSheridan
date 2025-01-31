c009e9ec477e79136ae9b8d51565443f4e0481de
Jack Rowe <rowejac@sheridancollege.ca>
2025-01-31 15:10:56 -0500

    Added constructor for the "CardGame" class

--- CardGame_Interactive/CardGameInteractive/CardGame.cs
+++ CardGame_Interactive/CardGameInteractive/CardGame.cs
@@ -2,16 +2,25 @@ namespace CardGameInteractive;
 
 public class CardGame
 {
-    //Represents the deck of card the game will use
+    //Represents the deck of card the game will use.
     private CardDeck _cardDeck;
     
     //The Scoring System of the game.
     private Score _score;
     
-    //Last card played by the user
+    //Last card played by the user.
     private Card _playerCard;
     
-    //Last card played by the house
+    //Last card played by the house.
     private Card _houseCard;
+    
+    //Constructor of the "CardGame" class.
+    public CardGame()
+    {
+        _cardDeck = new CardDeck();
+        _score = new Score();
+        _playerCard = null;
+        _houseCard = null;
+    }
 
 }
